988d53352c31bc0c1d134509a5bdcec0415bb419
zfreeman <zfreeman@lexmark.com>
2015-03-29 18:09:33 -0400

    Add command line version of hash playground

---
+++ hash.swift
@@ -0,0 +1,65 @@
+#!/usr/bin/env xcrun swift 
+import Cocoa
+import Foundation
+
+let expectedHashValue = 664523139931
+
+func hash(theString: String) -> Int {
+    var h : Int = 7;
+    let multiplier : Int = 37;
+    let letters = "acdegilmnoprstuw"
+    for var i=0; i < countElements(theString); i++ {
+        let stringIndex = advance(letters.startIndex, i)
+        var characterToFind = theString[stringIndex];
+        if var characterIndex = find(letters, characterToFind) {
+            var characterPosition = distance(letters.startIndex, characterIndex)
+            h = h * multiplier + characterPosition
+        }
+    }
+    return h;
+}
+
+func findStringForExpectedHashValue() -> Void {
+    let letters = "acdegilmnoprstuw"
+    var characterSet = Array(letters)
+    let desiredLength = 7
+    findAllNLength(characterSet, desiredLength)
+}
+
+//  mainly a wrapper over recursive function printAllNLengthRec()
+func findAllNLength(characterSet: [Character], desiredLength: Int) -> Void {
+    var numberOfCharacters = countElements(characterSet)
+    findAllNLengthRec(characterSet, "", numberOfCharacters, desiredLength);
+}
+
+// The main recursive method to print all possible strings of length n
+func findAllNLengthRec(characterSet: [Character], prefix: String, numberOfCharacters: Int, desiredLength: Int) -> Void{
+    
+    // Base case: n is 0, print prefix
+    if (desiredLength == 0) {
+        var actualHashValue = hash(prefix)
+        if (actualHashValue == expectedHashValue) {
+            println("Success")
+            println(prefix)
+            exit(0)
+        } else {
+            //print("Failure")
+        }
+        return;
+    }
+    
+    // One by one add all characters from set and recursively
+    // call for n equals to n-1
+    for var i = 0; i < numberOfCharacters; ++i {
+        
+        // Next character of input added
+        var newPrefixIndex = advance(characterSet.startIndex, i)
+        var newPrefix = prefix + [characterSet[newPrefixIndex]];
+        
+        // n is decreased, because we have added a new character
+        findAllNLengthRec(characterSet, newPrefix, numberOfCharacters, desiredLength - 1);
+    }
+}
+
+findStringForExpectedHashValue()
+
